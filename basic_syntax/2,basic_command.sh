b896996fc59b7982f6a5813bb3ad663f88fc9ec4
Choi-Moori <anflwkd@gmail.com>
2024-05-29 15:02:19 +0900

    vi commit this is vi commit

--- basic_syntax/2,basic_command.sh
+++ basic_syntax/2,basic_command.sh
@@ -1,2 +1,11 @@
-# 현재 working directory, staging area 상태 확인
+# 현재 working directory, staging area 만 상태 확인
 git status
+
+# git add : 모든 수정/추가사항 add
+git add .
+
+# git add 특정파일 : 특정 파일 add
+git add 특정파일(위치포함 tab 키)
+
+# commit 이력 확인
+git log 
